06c9025d39d1dc80162c14b0f4ed8b0c30eac4be
ev3con <ev3con@t-online.de>
2000-01-01 01:09:05 +0100

    Startskripte entfernt

--- start_af.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python /home/ev3con/python-ev3con/alternativ/autonomes_fahren.py
-
-sleep 3
--- start_af_platooning.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python /home/ev3con/python-ev3con/alternativ/autonomes_fahren_platooning.py -start_idle
-
-sleep 3
