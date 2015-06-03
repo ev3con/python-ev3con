25f76f45cd855abf9fbb38f19c127222f5677cff
ev3con <ev3con@t-online.de>
2015-06-03 21:20:56 +0200

    Update der Bluetooth-Skripte

---
+++ connect/bt_blink_server.py
@@ -0,0 +1,38 @@
+import time
+import bluetooth
+
+port = 1
+tgl = True
+server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server.bind(("", port))
+server.listen(1)
+server.setblocking(False)
+peers = []
+
+while True:
+    try:
+        bufsock, bufaddr = server.accept()
+        peers.append(bufsock)
+        port = port + 1
+        print "Neuer Teilnehmer: " + bufaddr[0] + " (" + bluetooth.lookup_name(bufaddr[0], timeout=3) + ")"
+    except:
+        pass
+
+    start_time = time.time()
+
+    for p in peers:
+        try:
+            if tgl:
+                p.send("on")
+            else:
+                p.send("off")
+        except:
+            peers.remove(p)
+
+    end_time = time.time()
+
+    print "Sendezeit: " + str( end_time - start_time ) + " bei " + str(len(peers)) + " Verbindungen"
+
+    tgl = not tgl
+
+    time.sleep(0.5)
--- connect/bt_discover.py
+++ connect/bt_discover.py
@@ -2,7 +2,7 @@ import bluetooth
 
 btdev = bluetooth.discover_devices(duration=5, lookup_names=True)
 
-print "Geraete gelesen"
+print "Available devices:"
 
 for dev in btdev:
     print dev
