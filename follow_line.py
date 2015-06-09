499c27212e913ce56bbda91c8ff16b0301933159
patrick <patrick@thinkpat>
2015-06-09 16:32:47 +0200

    Update des Linienverfolgers (ddemidov)

--- follow_line.py
+++ follow_line.py
@@ -1,3 +1,13 @@
+# line_line.py - Automatische Linienverfolung mit Abstandshaltung
+# 9. Juni 2015 - Patrick Ziegler, TU Dresden
+#
+# Dieses Skript stellt einen Machbarkeitstest der gleichzeitigen
+# Regelung von Linienverfolgung und Abstandshaltung dar. Es wurde
+# nicht die Library von topikatchu, sondern 'python-ev3dev' von
+# Denis Demidov verwendet!
+# Ein Schleifenzyklus dauert in der Regel 7-8 ms, in unregelmaessigen
+# Abstaenden dauert der Schleifendurchlauf bis zu 40 ms.
+
 import sys, time, traceback, argparse, math
 from ev3dev import *
 
@@ -9,39 +19,39 @@ class PID:
         self.lasterror = 0.0
         self.integral = 0.0
         self.derivative = 0.0
-        self.corr_follow = 0.0
+        self.corr_line = 0.0
 
     def calc(self, error, limit=None):
         self.integral = 0.5 * self.integral + error
         self.derivative = error - self.lasterror
         self.preverr = error
-        self.corr_follow = self.Kp * error + self.Ki * self.integral + self.Kd * self.derivative
+        self.corr_line = self.Kp * error + self.Ki * self.integral + self.Kd * self.derivative
 
         if limit == None:
-            return self.corr_follow
+            return self.corr_line
 
-        if abs(self.corr_follow) < abs(limit):
-            return self.corr_follow
+        if abs(self.corr_line) < abs(limit):
+            return self.corr_line
 
-        return (self.corr_follow / abs(self.corr_follow)) * limit
+        return (self.corr_line / abs(self.corr_line)) * limit
 
 if __name__ == "__main__":
     sound.beep()
     start_t = 0.0
 
     parser = argparse.ArgumentParser(sys.argv[0])
-    parser.add_argument('--Vref', dest='Vref', type=float, default=300)
-    parser.add_argument('--fKp', dest='fKp', type=float, default=4.0)
+    parser.add_argument('--Vref', dest='Vref', type=float, default=350)
+    parser.add_argument('--fKp', dest='fKp', type=float, default=2.5)
     parser.add_argument('--fKi', dest='fKi', type=float, default=0.0)
     parser.add_argument('--fKd', dest='fKd', type=float, default=9.0)
-    parser.add_argument('--dKp', dest='dKp', type=float, default=0.0)
+    parser.add_argument('--dKp', dest='dKp', type=float, default=3.0)
     parser.add_argument('--dKi', dest='dKi', type=float, default=0.0)
     parser.add_argument('--dKd', dest='dKd', type=float, default=0.0)
     args = parser.parse_args(sys.argv[1:])
 
-    pid_follow = PID(args.fKp, args.fKi, args.fKd)
+    pid_line = PID(args.fKp, args.fKi, args.fKd)
     pid_dist = PID(args.dKp, args.dKi, args.dKd)
-    corr_follow = 0.0
+    corr_line = 0.0
     corr_dist = 0.0
 
     ml = large_motor(OUTPUT_A)
@@ -53,11 +63,11 @@ if __name__ == "__main__":
 
     cs = color_sensor()
     cs.mode = 'COL-REFLECT'
-    colref = 18
+    colref = 30 # Ermittelt am Tisch in BAR I/48
 
     us = ultrasonic_sensor()
     us.mode = 'US-DIST-CM'
-    distref = 20
+    distref = 200
 
     try:
         ml.run_forever(speed_sp=int(args.Vref))
@@ -66,17 +76,17 @@ if __name__ == "__main__":
         while True:
             start_t = time.time()
 
-            corr_follow = pid_follow.calc(colref - cs.value(), 2*args.Vref)
+            corr_line = pid_line.calc(colref - cs.value(), 2*args.Vref)
             corr_dist = pid_dist.calc(distref - us.value(), 2*args.Vref)
 
-            if corr_dist > 0:
+            if corr_dist < 0:
                 corr_dist = 0
 
-            if corr_follow > 0: # im dunklen Bereich
+            if corr_line > 0: # im dunklen Bereich
                 ml.run_forever( speed_sp = int(args.Vref - corr_dist) )
-                mr.run_forever( speed_sp = int(args.Vref - corr_dist - corr_follow) )
-            elif corr_follow < 0: # im hellen Bereich
-                ml.run_forever( speed_sp = int(args.Vref - corr_dist + corr_follow) )
+                mr.run_forever( speed_sp = int(args.Vref - corr_dist - corr_line) )
+            elif corr_line < 0: # im hellen Bereich
+                ml.run_forever( speed_sp = int(args.Vref - corr_dist + corr_line) )
                 mr.run_forever( speed_sp = int(args.Vref - corr_dist) )
 
             print "Schleifendauer: " + str(time.time() - start_t) + "s"
