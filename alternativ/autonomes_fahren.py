bd512a9c982fdcd956884664492bc06a632540fa
ev3con <ev3con@t-online.de>
2015-07-08 08:38:41 +0200

    Alternatives autonomes_fahren hinzugefügt

---
+++ alternativ/autonomes_fahren.py
@@ -0,0 +1,118 @@
+import sys, time, traceback, argparse
+from ev3.ev3dev import *
+from ev3.lego import *
+
+class PID:
+    def __init__(self, Kp=0.0, Ki=0.0, Kd=0.0):
+        self.reset(Kp, Ki, Kd)
+
+    def reset(self, Kp=0.0, Ki=0.0, Kd=0.0):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.lasterror = 0.0
+        self.integral = 0.0
+        self.derivative = 0.0
+        self.correction = 0.0
+
+    def calc(self, error, limit=None):
+        self.integral = 0.5 * self.integral + error
+        self.derivative = error - self.lasterror
+        self.lasterror = error
+        self.correction = self.Kp * error + self.Ki * self.integral + self.Kd * self.derivative
+
+        if not limit == None:
+            if abs(self.correction) > abs(limit):
+                self.correction = (self.correction / abs(self.correction)) * limit
+
+        return self.correction
+
+def follow_line(Vref=0.0, colmax=0.0, colmin=0.0, distref=0.0, lKp=0.0, lKi=0.0, lKd=0.0, dKp=0.0, dKi=0.0, dKd=0.0):
+    cycle_t = 0.0
+
+    pid_line = PID(lKp, lKi, lKd)
+    pid_dist = PID(dKp, dKi, dKd)
+
+    ml = Motor(port=Motor.PORT.A)
+    mr = Motor(port=Motor.PORT.B)
+
+    cs = ColorSensor()
+    us = UltrasonicSensor()
+    hupe = Tone()
+
+    led = LED()
+    led.left.color = LED.COLOR.AMBER
+    led.right.color = LED.COLOR.AMBER
+
+    try:
+        ml.run_forever(Vref, speed_regulation=True)
+        mr.run_forever(Vref, speed_regulation=True)
+
+        while True:
+            cycle_t = time.time()
+
+            # die Farbwerte werden automatisch auf Bereich [0,100] abgebildet, Differenz zu 50 ist Fehler fuer PID
+            pid_line.calc( 50 - ( ( cs.reflect - colmin ) / ( colmax - colmin ) ) * 100 )
+            pid_dist.calc( distref - us.dist_cm )
+
+            # es soll nicht auf Hindernis zubeschleunigt werden
+            if pid_dist.correction < 0:
+                pid_dist.correction = 0
+
+            # Motoren werden invertiert angesteuert, da sie in anderer Richtung verbaut sind
+            if pid_line.correction > 0: # im dunklen Bereich
+                ml.run_forever( (-1) * ( Vref - pid_dist.correction ) )
+                mr.run_forever( (-1) * ( Vref - pid_dist.correction - pid_line.correction ) )
+            elif pid_line.correction < 0: # im hellen Bereich
+                ml.run_forever( (-1) * ( Vref - pid_dist.correction + pid_line.correction ) )
+                mr.run_forever( (-1) * ( Vref - pid_dist.correction ) )
+
+            # Startzeit des Zuckelns merken
+            if abs( Vref - pid_dist.correction ) < 100 and not standing_t:
+                standing_t = time.time()
+
+            if abs( Vref - pid_dist.correction ) > 100 and standing_t:
+                standing_t = 0
+
+            # Fahrtsteuerung komplett abschalten, wenn mind. 1 Sekunde gezuckelt wurde
+            if standing_t and ( time.time() - standing_t ) > 1:
+                ml.stop()
+                mr.stop()
+                led.left.color = LED.COLOR.RED
+                led.right.color = LED.COLOR.RED
+                hupe.play(100,500)
+                return
+
+            print "Zyklusdauer: " + str( ( time.time() - cycle_t ) * 1000 ) + "ms"
+
+    except:
+        ml.stop()
+        mr.stop()
+
+        print traceback.format_exc()
+        print "Programm wurde beendet"
+
+def wait_barrier(distref=0.0):
+    us = UltrasonicSensor()
+
+    while True:
+        if us.dist_cm > distref:
+            return
+
+        time.sleep(0.03)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser( sys.argv[0] )
+    parser.add_argument( '-Vref', dest='Vref', type=float, default=350 )
+    parser.add_argument( '-colmax', dest='colmax', type=float, default=63.0 )
+    parser.add_argument( '-colmin', dest='colmin', type=float, default=7.0 )
+    parser.add_argument( '-distref', dest='distref', type=float, default=30.0 ) # Abstand in cm
+    parser.add_argument( '-lKp', dest='lKp', type=float, default=3.5 )
+    parser.add_argument( '-lKi', dest='lKi', type=float, default=0.0 )
+    parser.add_argument( '-lKd', dest='lKd', type=float, default=2.0 )
+    parser.add_argument( '-dKp', dest='dKp', type=float, default=30.0 )
+    parser.add_argument( '-dKi', dest='dKi', type=float, default=0.0 )
+    parser.add_argument( '-dKd', dest='dKd', type=float, default=0.0 )
+    args = parser.parse_args( sys.argv[1:] )
+
+    follow_line(args.Vref, args.colmax, args.colmin, args.distref, args.lKp, args.lKi, args.lKd, args.dKp, args.dKi, args.dKd)
---
+++ alternativ/autonomes_fahren_hopbyhop.py
@@ -0,0 +1,78 @@
+import sys, argparse, socket
+from multiprocessing import Process
+from ev3.ev3dev import Motor
+from autonomes_fahren import *
+
+# Beim Aufruf uebergebene Argumente verarbeiten
+parser = argparse.ArgumentParser( sys.argv[0] )
+parser.add_argument( "-Vref", dest="Vref", type=float, default=350 )
+parser.add_argument( "-colmax", dest="colmax", type=float, default=63.0 )
+parser.add_argument( "-colmin", dest="colmin", type=float, default=7.0 )
+parser.add_argument( "-distref", dest="distref", type=float, default=30.0 ) # Abstand in cm
+parser.add_argument( "-lKp", dest="lKp", type=float, default=3.5 )
+parser.add_argument( "-lKi", dest="lKi", type=float, default=0.0 )
+parser.add_argument( "-lKd", dest="lKd", type=float, default=2.0 )
+parser.add_argument( "-dKp", dest="dKp", type=float, default=30.0 )
+parser.add_argument( "-dKi", dest="dKi", type=float, default=0.0 )
+parser.add_argument( "-dKd", dest="dKd", type=float, default=0.0 )
+parser.add_argument( "-ip", dest="ip", type=str, default="127.0.0.1" )
+args = parser.parse_args( sys.argv[1:] )
+fl_args = (args.Vref, args.colmax, args.colmin, args.distref, args.lKp, args.lKi, args.lKd, args.dKp, args.dKi, args.dKd)
+
+# IP-Adresse des Verfolgers berechnen
+purs_ip = args.ip.split(".")
+purs_ip = purs_ip[0] + "." + purs_ip[1] + "." purs_ip[2] + "." + str( abs( int(purs_ip[3] - 1 ) )
+
+# Socket erstellen und an eigene IP binden
+sock = socket.socket(type=socket.SOCK_DGRAM)
+sock.bind((args.ip, 5005))
+sock.settimeout(0.6)
+
+# Fahrt beginnen, Steuerungsprozess wird parallel gestartet
+p = Process( name="follow_line", target=follow_line, args=fl_args)
+p.start()
+
+# Vielleicht muessen Motoren manuell gestoppt werden
+# ml = Motor(port=Motor.PORT.A)
+# mr = Motor(port=Motor.PORT.B)
+
+# Endlosschleife, darin erfolgt die Kommunikation und die Verwaltung des Steuerungsprozesses
+while True:
+    try:
+        mesg, addr = sock.recvfrom(1024)
+    except socket.timeout:
+        pass
+
+    # Falls kein ACK empfangen wurde, gehen wir von einem STOP aus
+    if not mesg.startswith("ACK"):
+        sock.sendto("ACK", addr)
+        if p.name() == "follow_line":
+            p.terminate()
+            # ml.stop()
+            # mr.stop()
+
+    # Der Steuerungsprozess wird ggf. mit Warteprozess ausgetauscht, wenn Hindernis vorhanden
+    if not p.is_alive():
+        if p.name() == "follow_line":
+
+            # Drei Versuche, ein STOP an den Verfolger zu senden
+            for i in range(0,3):
+                sock.sendto("STOP", (purs_ip, 5005))
+
+                try:
+                    mesg, addr = sock.recvfrom(1024)
+                except socket.timeout:
+                    pass
+
+                if mesg.startswith("ACK"):
+                    break
+
+            # Warteprozess wird gestartet; dieser wartet, bis Hindernis verschwindet
+            p = Process(name="wait_barrier", target=wait_barrier, args=(0.5*args.distref,))
+            p.start()
+
+        else:
+            # Hindernis ist weg (den Warteprozess war tot), es kann weitergefahren werden
+            p = Process(name="follow_line", target=follow_line, args=fl_args)
+            p.start()
+
